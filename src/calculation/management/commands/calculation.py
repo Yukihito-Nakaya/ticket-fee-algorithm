8629ce3e87fa38c985bd7feaa2dac5d30e3b5827
Yukihito Nakaya <>
2024-11-19 17:09:47 +0900

    料金アルゴリズムの修正、割引適用アルゴリズムの作成、出力項目追加

--- src/calculation/management/commands/calculation.py
+++ src/calculation/management/commands/calculation.py
@@ -14,7 +14,7 @@ class RateTable:
         return self.rates.get(0)
     
     def get_discount_rate(self,discount):
-        return {self.rates.get(0),self.rates.get(discount,'discountコードが間違っています。')}
+        return self.rates.get(discount)
 
 #料金計算アルゴリズム
 class Command(BaseCommand):
@@ -28,11 +28,17 @@ class Command(BaseCommand):
         rate_table = RateTable()
         rate = rate_table.get_base_rate()
 
-        #total_base
+        #料金明細用
+        charge_details_data = {}
+        #料金明細差額一時保存変数
+        disvalue = 0
+
+        #金額変更前の合計料金
         total_base = (options['adults'] * rate.get('adult'))
         + (options['childs'] * rate.get('child'))
         + (options['seniors'] * rate.get('senior'))
 
+        #販売合計金額
         sales_amount = total_base
         
         #特別タイプの場合
@@ -41,31 +47,53 @@ class Command(BaseCommand):
             sales_amount = (options['adults'] * rate.get('adult'))
             + (options['childs'] * rate.get('child'))
             + (options['seniors'] * rate.get('senior'))
-
+            charge_details_data["タイプ割引価格"] = [
+                f"大人:{options['adults']}×{rate.get('adult')}",
+                f"子供:{options['childs']}×{rate.get('child')}",
+                f"シニア:{options['seniors']}×{rate.get('senior')}",
+                f"{sales_amount}"
+                ]
+            
         #団体割引確認
         if (options['adults']+ (options['childs'] * 0.5) + options['seniors']) >= 10:
-            sales_amount = sales_amount * 0.9
+            disvalue = sales_amount * 0.9
+            sales_amount = disvalue
+
+            charge_details_data["団体割引"] = ["-10%",f"-{disvalue}"]
 
         #時間割引確認
         if now.hour >= 17:
-            sales_amount = - (options['adults']+ options['childs'] + options['seniors']) * 100
+            disvalue = (options['adults']+ options['childs'] + options['seniors']) * 100
+            sales_amount = sales_amount - disvalue
+
+            charge_details_data["時間割引"] = [f"-{options['adults']+ options['childs'] + options['seniors']}×100円",f"-{disvalue}"]
+
 
         #休日確認
-        if weekday == 5 or weekday == 6 or options['holiday']:
-            sales_amount = sales_amount * 0.8
+        if weekday == 5 or weekday == 6 or options['holiday'] == 1:
+            disvalue = (options['adults']+ options['childs'] + options['seniors']) * 200
+            sales_amount = sales_amount + disvalue 
+
+            charge_details_data["休日料金"] = [f"+{options['adults']+ options['childs'] + options['seniors']}×200円",f"+{disvalue}"]
 
         #曜日割引確認
         if weekday == 0 or weekday == 2:
-            sales_amount = sales_amount - (options['adults']+ options['childs'] + options['seniors']) * 100
+            disvalue = (options['adults']+ options['childs'] + options['seniors']) * 100
+            sales_amount = sales_amount - disvalue
 
-        
+            charge_details_data["曜日割引"] = [f"-{options['adults']+ options['childs'] + options['seniors']}×100円",f"-{disvalue}"]
+
+        print(weekday)
         print(f"販売合計金額:{sales_amount}円")
         print(f"金額変更前合計金額:{total_base}円")
-        print(f"販売日付:{now.strftime('%Y-%m-%d %H:%M:%S')}", f"曜日:{weekday}")
+        print("料金明細")
+        print("**********************")
+        print(f"販売日付:{now.strftime('%Y-%m-%d %H:%M:%S')}")
+        print(charge_details_data)
     
     def add_arguments(self, parser):
         parser.add_argument('adults', type=int)
         parser.add_argument('childs', type=int)
         parser.add_argument('seniors', type=int)
         parser.add_argument('--discount',default = 0,type=int)
-        parser.add_argument('--holiday',default = False, type=bool)
+        parser.add_argument('--holiday',default = 0, type=int)
