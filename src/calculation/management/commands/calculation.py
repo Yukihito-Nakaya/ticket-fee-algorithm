f8cca05f25ce46301a0e5a40ec6ddaa9c7dbe41a
Yukihito Nakaya <developer.nakaya@gmail.com>
2024-11-19 13:37:32 +0900

    readme 不明点追加 課金アルゴリズム追加

--- src/calculation/management/commands/calculation.py
+++ src/calculation/management/commands/calculation.py
@@ -32,21 +32,40 @@ class Command(BaseCommand):
         total_base = (options['adults'] * rate.get('adult'))
         + (options['childs'] * rate.get('child'))
         + (options['seniors'] * rate.get('senior'))
+
+        sales_amount = total_base
         
         #特別タイプの場合
         if options['discount'] > 0:
             rate = rate_table.get_discount_rate(options['discount'])
+            sales_amount = (options['adults'] * rate.get('adult'))
+            + (options['childs'] * rate.get('child'))
+            + (options['seniors'] * rate.get('senior'))
 
         #団体割引確認
         if (options['adults']+ (options['childs'] * 0.5) + options['seniors']) >= 10:
-            discount_base = total_base * 0.9
+            sales_amount = sales_amount * 0.9
+
+        #時間割引確認
+        if now.hour >= 17:
+            sales_amount = - (options['adults']+ options['childs'] + options['seniors']) * 100
+
+        #休日確認
+        if weekday == 5 or weekday == 6 or options['holiday']:
+            sales_amount = sales_amount * 0.8
+
+        #曜日割引確認
+        if weekday == 0 or weekday == 2:
+            sales_amount = sales_amount - (options['adults']+ options['childs'] + options['seniors']) * 100
+
         
-        print(f"金額変更前合計金額:{total_base}")
-        print(f"実行日付:{now.strftime('%Y-%m-%d %H:%M:%S')}", f"曜日:{weekday}")
-        print(options['childs'],  options['seniors'])
+        print(f"販売合計金額:{sales_amount}円")
+        print(f"金額変更前合計金額:{total_base}円")
+        print(f"販売日付:{now.strftime('%Y-%m-%d %H:%M:%S')}", f"曜日:{weekday}")
     
     def add_arguments(self, parser):
         parser.add_argument('adults', type=int)
         parser.add_argument('childs', type=int)
         parser.add_argument('seniors', type=int)
         parser.add_argument('--discount',default = 0,type=int)
+        parser.add_argument('--holiday',default = False, type=bool)
